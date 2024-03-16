480e88800d3cad3572d036167d73afc5a61f8450
J. Adrian Ruiz C. <ed.alanpoeta@gmail.com>
2024-03-16 13:12:51 -0600

    Mongoose installed and configured correctly

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Express working!");
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Successful connection to MongoDB"))
+  .catch((err) => console.log("Error connection to MongoDB", err));
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
